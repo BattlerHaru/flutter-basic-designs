833eb125d0117c1ca4e7b99a18d645695dae7c11
V-Haru <kronk.sdance@gmail.com>
2021-01-21 13:50:20 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:disenios/src/pages/basico_page.dart';
+import 'package:disenios/src/pages/scroll_page.dart';
+import 'package:disenios/src/pages/botones_page.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isAndroid) {
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
+          .copyWith(statusBarColor: Colors.transparent));
+    } else if (Platform.isIOS) {
+      SystemChrome.setSystemUIOverlayStyle(
+          SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.white));
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Diseños',
+      initialRoute: 'botones',
+      routes: {
+        'basico': (BuildContext context) => BasicoPage(),
+        'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage(),
+      },
+    );
+  }
+}
---
+++ lib/src/pages/basico_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubtitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearImagen() {
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage('<YOUR LINK IMAGE>'),
+        height: 200.0,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _crearTitulo() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'TEXT TITLE',
+                    style: estiloTitulo,
+                  ),
+                  SizedBox(
+                    height: 7.0,
+                  ),
+                  Text(
+                    'Text subtitle',
+                    style: estiloSubtitulo,
+                  ),
+                ],
+              ),
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.red,
+              size: 30.0,
+            ),
+            Text(
+              '41',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(Icons.call, 'Call'),
+        _accion(Icons.near_me, 'Route'),
+        _accion(Icons.share, 'Share'),
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 40.0,
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          texto.toUpperCase(),
+          style: TextStyle(fontSize: 15.0, color: Colors.blue),
+        ),
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0),
+        child: Text(
+          'Reprehenderit labore est ipsum deserunt aliqua qui dolor excepteur mollit aute dolore labore. Consectetur aliquip ipsum tempor sunt fugiat nostrud commodo occaecat deserunt pariatur cupidatat officia. Consectetur ullamco sit commodo ea velit. Aute irure aute dolor aute tempor nulla velit nostrud nisi est ullamco quis. Exercitation incididunt proident anim mollit consequat velit eiusmod culpa elit magna duis mollit fugiat.',
+          textAlign: TextAlign.justify,
+        ),
+      ),
+    );
+  }
+}
